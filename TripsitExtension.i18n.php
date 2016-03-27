501e23d4baff10c2670e38b04bb900016c48291e
reality <reality@tripsit.me>
2016-03-27 18:33:03 +0000

    working version

---
+++ TripsitExtension.i18n.php
@@ -0,0 +1,10 @@
+<?php
+
+$magicWords = array();
+
+/** English
+ * @author reality
+ */
+$magicWords['en'] = array(
+   'tdose' => array( 0, 'tdose' )
+);
--- TripsitExtension.php
+++ TripsitExtension.php
@@ -1,36 +1,46 @@
 <?php
 
-$wgExtensionCredits['parserhook'][] = array(
-  'path' => _FILE_,
+$wgExtensionCredits['tripsitextension'][] = array(
+  'path' => __FILE__,
   'name' => 'TripSit Mediawiki Extension',
   'description' => 'Pull information from TripSit factsheets into a wiki',
   'version' => '0.0.1',
-  'author' => 'reality'
+  'author' => 'reality',
+  'url' => 'test'
 );
 
-$wgHooks['ParserFirstCallInit'][] = 'ExampleExtension::onParserSetup';
-
-$wgExtensionMessageFiles['TripsitExtension'][] = 'TripsitExtension::onParserSetup';
+$wgExtensionMessagesFiles['TripsitExtension'] = __DIR__ . '/TripsitExtension.i18n.php';
+$wgHooks['ParserFirstCallInit'][] = 'TripsitExtension::onParserSetup';
 
 class TripsitExtension {
   function onParserSetup(&$parser) {
-    $parser->setFunctionHook('t_dose', 'TripsitExtension::renderDose');
+    $parser->setFunctionHook('tdose', 'TripsitExtension::renderDose');
   }
 
-  function renderDose($parser, $drug='') {
-    $result = file_get_contents('http://drugs.tripsit.me/raw/'+$drug);
+  function renderDose($parser, $param1='') {
+    $parser->disableCache();
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_URL, 'http://drugs.tripsit.me/raw/'.$param1);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type:application/json'));
+	curl_setopt($ch, CURLOPT_HEADER, false);
+	curl_setopt($ch, CURLOPT_POST, false);
+	$result = curl_exec($ch);
+	curl_close($ch);
+
     $drug = json_decode($result);
 
-    $output = '';
+	$output = '';
 
     foreach($drug->formatted_dose as $roa => $dose_expression) {
-      $output .= '{| class="wikitable"\n';
-      $output .= '|+' . $roa . '\n';
+      $output .= "{| class=\"wikitable\"\n";
+      $output .= "|+" . $roa . "\n";
       foreach($dose_expression as $level => $level_dose) {
-        $output .= '|-\n';
-        $output .= '| ' . $level . ' || ' + $level_dose + '\n';
+        $output .= "|-\n";
+        $output .= "| " . $level . " || " . $level_dose . "\n";
       }
-      $output .= '|}\n';
+      $output .= "|}\n";
     }
 
     return $output;
